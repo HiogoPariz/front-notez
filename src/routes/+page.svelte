e25f6719aef27ef74e09a5eb2b2c38bda8b1eba1
Hiogo de Pariz Cavalheiro <hiogopariz@gmail.com>
2024-07-08 09:35:40 -0300

    feat: editorjs

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,41 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	// @ts-nocheck
+	import { onMount } from 'svelte';
+
+	let editor;
+
+	onMount(async () => {
+		const EditorJS = (await import('@editorjs/editorjs')).default;
+		const Header = (await import('@editorjs/header')).default;
+		const Checklist = (await import('@editorjs/checklist')).default;
+		const List = (await import('@editorjs/list')).default;
+		const Quote = (await import('@editorjs/quote')).default;
+
+		editor = new EditorJS({
+			holder: 'editor',
+			tools: {
+				header: Header,
+				checklist: {
+					class: Checklist,
+					inlineToolbar: true,
+                    shortcut: 'CMD+SHIFT+C',
+				},
+				list: {
+					class: List,
+					inlineToolbar: true,
+                    shortcut: 'CMD+SHIFT+L',
+					config: {
+						defaultStyle: 'unordered'
+					}
+				},
+				quote: {
+					class: Quote,
+					inlineToolbar: true,
+					shortcut: 'CMD+SHIFT+O'
+				}
+			}
+		});
+	});
+</script>
+
+<div id="editor" class=""></div>
