a10f12f4056d6acd20c8cdc1b7cc4910d36ec1ca
Super Wombo <rafaelabres@gmail.com>
2024-07-13 20:56:50 -0300

    feat: autosave(throttle)

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,4 @@
-import js from '@eslint/js';
+ import js from '@eslint/js';
 import svelte from 'eslint-plugin-svelte';
 import prettier from 'eslint-config-prettier';
 import globals from 'globals';
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,29 +1,60 @@
 <script>
 	// @ts-nocheck
 	import { onMount } from 'svelte';
+	import throttle from 'lodash.throttle';
+	let editor
 
-	let editor;
+	// AUTO SAVE FUNCTION
+	// TODO DEFINE MS
+	// DEFINE IP PROTOCOL
+	const delay = 300;
+	const throttleKeydownSave = throttle(() => {
+		save();
+	}, delay);
 
-	onMount(async () => {
-		const EditorJS = (await import('@editorjs/editorjs')).default;
-		const Header = (await import('@editorjs/header')).default;
-		const Checklist = (await import('@editorjs/checklist')).default;
-		const List = (await import('@editorjs/list')).default;
-		const Quote = (await import('@editorjs/quote')).default;
+	const handleKeydown = (e) => {
+		throttleKeydownSave(e);
+	};
 
-		editor = new EditorJS({
+	async function save() {
+		const url = 'http://0.0.0.0:5432/autosave';
+		try {
+			const data = await editor.save();
+			console.log({data})
+			const res = await fetch(url, {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				body: JSON.stringify(data)
+			});
+			const json = await res.json();
+		} catch (error) {
+			console.error('ERROR:', error);
+		}
+	}
+
+	onMount(
+		async () => {
+			const EditorJS = (await import('@editorjs/editorjs')).default;
+			const Header = (await import('@editorjs/header')).default;
+			const Checklist = (await import('@editorjs/checklist')).default;
+			const List = (await import('@editorjs/list')).default;
+			const Quote = (await import('@editorjs/quote')).default;
+			editor = new EditorJS({
+			autofocus: true,
 			holder: 'editor',
 			tools: {
 				header: Header,
 				checklist: {
 					class: Checklist,
 					inlineToolbar: true,
-                    shortcut: 'CMD+SHIFT+C',
+		            shortcut: 'CMD+SHIFT+C',
 				},
 				list: {
 					class: List,
 					inlineToolbar: true,
-                    shortcut: 'CMD+SHIFT+L',
+		            shortcut: 'CMD+SHIFT+L',
 					config: {
 						defaultStyle: 'unordered'
 					}
@@ -32,10 +63,12 @@
 					class: Quote,
 					inlineToolbar: true,
 					shortcut: 'CMD+SHIFT+O'
-				}
+				},
 			}
-		});
-	});
+		})
+		}
+	);
 </script>
 
-<div id="editor" class=""></div>
+<svelte:window on:keydown={handleKeydown} />
+<div class="editor" id="editor"/>
